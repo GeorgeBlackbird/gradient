00512187750d75607e8756a4c644834bc5fba0b9
GeorgeBlackbird <drozdov.jorj@yandex.ru>
2026-05-31 22:51:17 +0500

    chore: add vitest, prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: [
-    '@nuxt/eslint',
-    '@vueuse/nuxt',
-  ]
+  modules: ['@nuxt/eslint', '@vueuse/nuxt'],
 })
